f6ecbc7017c4d0d0248439b092ed37354921c6b2
andrewwer416 <awernersbach0361@floridapoly.edu>
2022-10-06 19:55:53 -0400

    Finished initial bug testing and seemingly functional

--- MinesweeperAI/Minesweeper.cpp
+++ MinesweeperAI/Minesweeper.cpp
@@ -78,62 +78,65 @@ public:
         adj_mine = false;
         bg_sprite.setTexture(TextureManager::GetTexture("tile_hidden"));
         fg_sprite.setTexture(TextureManager::GetTexture("tile_hidden"));
-        CheckAdjacents();
     }
     //setters
     void SetAdjacent(Tile* neighbor) {
-        if (!(find(adjacents.begin(), adjacents.end(), neighbor) != adjacents.end())) {
-            adjacents.push_back(neighbor);
-            if (neighbor->is_mine) {
-                adj_mine = true;
-            }
+        adjacents.push_back(neighbor);
+        if (neighbor->is_mine) {
+            number++;
+             adj_mine = true;
         }
     }
-    void SetFG() {
-        if (number != 0) {
+    void RevealFG() {
+        if (number != 0 && !IsAMine()) {
             fg_sprite.setTexture(TextureManager::GetTexture("number_" + to_string(number)));
+        }
+        else if (IsAMine()) {
+            fg_sprite.setTexture(TextureManager::GetTexture("mine"));
         }
         else {
             fg_sprite.setTexture(TextureManager::GetTexture("tile_revealed"));
         }
     }
-    void SetBG() {
+    void RevealBG() {
         bg_sprite.setTexture(TextureManager::GetTexture("tile_revealed"));
     }
     void SetMine() {
         is_mine = true;
-        bg_sprite.setTexture(TextureManager::GetTexture("mine"));
     }
     //actions
-    void RightClick() {
-        if (!is_flagged) {
+    void RightClick(int& score) {
+        if (!is_revealed && !is_flagged) {
             is_flagged = true;
             fg_sprite.setTexture(TextureManager::GetTexture("flag"));
+            score--;
         }
-        else {
+        else if(!is_revealed) {
             is_flagged = false;
             fg_sprite.setTexture(TextureManager::GetTexture("tile_hidden"));
+            score++;
         }
     }
-    int RevealTile(Tile& tile, int& curr_revealed) {
-        if (!is_revealed && !is_mine && !is_flagged) {
-            bg_sprite.setTexture(TextureManager::GetTexture("tile_revealed"));
-            is_revealed = true;
-            SetFG();
-            SetBG();
+    void RevealTile(Tile& tile, int& curr_revealed, bool& mine) {
+        if (!tile.is_revealed && !tile.is_mine && !tile.is_flagged) {
+            tile.is_revealed = true;
+            tile.RevealFG();
+            tile.RevealBG();
             curr_revealed++;
-            if (!adj_mine){
-                for (int i = 0; i < adjacents.size(); i++) {
-                    RevealTile(*adjacents[i], curr_revealed);
-                }
+            for (int i = 0; i < tile.adjacents.size(); i++) {
+                if (!tile.adj_mine && !tile.adjacents[i]->is_revealed) {
+                RevealTile(*tile.adjacents[i], curr_revealed, mine);
             }
-            return 0;
         }
-        else if (!is_revealed && !is_flagged) {
-            bg_sprite.setTexture(TextureManager::GetTexture("tile_revealed"));
-            is_revealed = true;
-            fg_sprite.setTexture(TextureManager::GetTexture("mine"));
-            return 1;
+    }
+        else if (!tile.is_revealed && !tile.is_flagged) {
+            tile.is_revealed = true;
+            tile.RevealFG();
+            tile.fg_sprite.setTexture(TextureManager::GetTexture("mine"));
+            tile.RevealBG();
+            mine = true;
+        }
+        else {
         }
     }
     bool checkBounds(sf::Vector2i mousePos) {
@@ -153,16 +156,6 @@ public:
     }
     bool IsAMine() {
         return is_mine;
-    }
-    void CheckAdjacents() {
-        for (int i = 0; i < adjacents.size(); i++) {
-            if (adjacents[i]->is_mine) {
-                cout << GetLocation().x << ", " << GetLocation().y << endl;
-                cout << adjacents[i]->GetLocation().x << ", " << adjacents[i]->GetLocation().y << endl << endl;;
-                number++;
-                adj_mine = true;
-            }
-        }
     }
     Point GetLocation() {
         return coordinates;
@@ -242,33 +235,46 @@ public:
     //setter
     void SetAdjacents() {
         for (int i = 0; i < board.size(); i++) {
+            //cout << "Tile at: (" << board[i].GetLocation().x << ", " << board[i].GetLocation().y << ") has adjacents: " << endl;
             for (int j = (i - num_cols - 1); j <= (i - num_cols + 1); j++) {
                 if (j >= 0 && j < board.size() && (board.at(i).GetLocation().y == board.at(j).GetLocation().y + 1)) {
+                    //cout << "(" << board[j].GetLocation().x << ", " << board[j].GetLocation().y << ")" << endl;
                     board.at(i).SetAdjacent(&board.at(j));
                 }
             }
             for (int j = (i - 1); j <= (i + 1); j += 2) {
                 if (j >= 0 && j < board.size() && (board.at(i).GetLocation().y == board.at(j).GetLocation().y)) {
+                    //cout << "(" << board[j].GetLocation().x << ", " << board[j].GetLocation().y << ")" << endl;
                     board.at(i).SetAdjacent(&board.at(j));
                 }
             }
             for (int j = (i + num_cols - 1); j <= (i + num_cols + 1); j++) {
                 if (j >= 0 && j < board.size() && (board.at(i).GetLocation().y == board.at(j).GetLocation().y - 1)) {
+                    //cout << "(" << board[j].GetLocation().x << ", " << board[j].GetLocation().y << ")" << endl;
                     board.at(i).SetAdjacent(&board.at(j));
                 }
             }
         }
     }
     //function to update tile at an index
-    void UpdateTile(int index, bool& leftClicked, bool& mine, bool& reveal) {
+    void UpdateTile(int index, bool& leftClicked, bool& mine, bool& reveal , int& score) {
         if (leftClicked) {
-            mine = board[index].RevealTile(board[index], num_revealed); // mine becomes 1 when a mine is revealed
+            board[index].RevealTile(board[index], num_revealed, mine); // mine becomes 1 when a mine is revealed
         }
         else {
-            board[index].RightClick();
+            board[index].RightClick(score);
         }
         if ((num_rows * num_cols) - num_revealed == num_mines) {
              reveal = true;
+        }
+    }
+    //troubleshooting function
+    void print_mines() {
+        for (int i = 0; i < board.size(); i++) {
+            if (board[i].IsAMine()) {
+                board[i].RevealFG();
+                board[i].RevealBG();
+            }
         }
     }
 private:
@@ -301,17 +307,23 @@ public:
         window.create(VideoMode(width, height), "Minesweeper");
         LoadAll();
         DrawMenu();
+        faceBounds = face.getGlobalBounds();
         Reset();
     }
+    //game loop of reloading the window and accepting input
     void GameLoop() {
+        //until game is closed
         while (window.isOpen()) {
             Event event;
             while (window.pollEvent(event)) {
+                //check if game is closed
                 if (event.type == Event::Closed) {
                     window.close();
                 }
                 bool leftClick;
+                //check if user input
                 if (event.type == sf::Event::MouseButtonPressed) {
+                    //left click: either menu interaction or tile interaction
                     if (event.mouseButton.button == sf::Mouse::Left) {
                         leftClick = true;
                         sf::Vector2i mousePos = sf::Mouse::getPosition(window);
@@ -319,46 +331,69 @@ public:
                             Reset();
                         }
                     }
-                    if (event.mouseButton.button == sf::Mouse::Right) {
-                        leftClick = false;
-                    }
-                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                    for (int i = 0; i < game_board.Get_Board().size(); i++) {
-                        if (game_board.Get_Board()[i].checkBounds(mousePos)) {
-                            game_board.UpdateTile(i, leftClick, mine_pressed, all_revealed);
+                    // only update board if game state == 0: not win or loss
+                    if (game_state == 0) {
+                        //right click: update flag status of a tile
+                        if (event.mouseButton.button == sf::Mouse::Right) {
+                            leftClick = false;
+                        }
+                        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                        // for each tile
+                        for (int i = 0; i < game_board.Get_Board().size(); i++) {
+                            //check if tile clicked
+                            if (game_board.Get_Board()[i].checkBounds(mousePos)) {
+                                //update clicked tile and works depending on right/left click
+                                game_board.UpdateTile(i, leftClick, mine_pressed, all_revealed, score);
+                                if (all_revealed) {
+                                    game_state = 1;
+                                }
+                                if (mine_pressed == true) {
+                                    game_state = -1;
+                                }
+                            }
                         }
                     }
                 }
             }
                 sf::Vector2i mousePos = sf::Mouse::getPosition(window);
                 window.clear(Color::White);
+                //draw every tile
                 for (int i = 0; i < game_board.Get_Rows(); i++) {
                     for (int j = 0; j < game_board.Get_Cols(); j++) {
                         window.draw(game_board.Get_Tile(j, i).GetBGSprite());
                         window.draw(game_board.Get_Tile(j, i).GetFGSprite());
-                    }  
+                    }
                 }
-                for (int i = 0; i < mines.size(); i++) {
-                    window.draw(mines.at(i));
-                }
+                //draw number menu
                 for (int i = 0; i  < digitDisplay.size(); i++) {
                     digitDisplay[i].setPosition(21 * i, game_board.Get_Rows() * 32);
                     window.draw(digitDisplay[i]);
                 }
+                //if lose: change face to :(
+                if (game_state == -1) {
+                    face.setTexture(TextureManager::GetTexture("face_lose"));
+                }
+                //if win: change face to B)
+                else if (game_state == 1) {
+                    face.setTexture(TextureManager::GetTexture("face_win"));
+                }
+                //change numbers based on score
+                SetDigitMap(score, digitDisplay, digitOptions);
+                /*
+                * FOR TESTING
+                * game_board.print_mines();
+                */
                 window.draw(face);
-                //window.draw(debug);
                 window.display();
         }
+        //clear texturemanager
         TextureManager::Clear();
     }
-    void PressTile(int action[], int size) {
-        
-    }
+    //reset function to set all game instance variables to default
     void Reset() {
         face.setTexture(TextureManager::GetTexture("face_happy"));
         //numbers.clear();
-        mines.clear();
-        flags.clear();
+        game_state = 0;
         mine_pressed = false;
         all_revealed = false;
         game_board.Reset();
@@ -366,31 +401,20 @@ public:
         SetDigitMap(score, digitDisplay, digitOptions);
     }
 private:
-    bool sign;
-    int dig1;
-    int dig2;
-    int dig3;
-    int nextMines;
     int width;
     int height;
     int score;
+    int game_state;
     int tilesRevealed;
     bool mine_pressed;
     bool all_revealed;
-    bool isDebugMode;
-    ifstream inFile;
-    vector<Sprite> mines;
     vector<Sprite>digitOptions;
-    map<int, Sprite> numbers;
-    map<int, Sprite> flags;
     vector<Sprite>digitDisplay;
-    vector<int> unrevealed;
-    Sprite face, debug, mine, flag;
+    Sprite face, mine, flag;
     Sprite digits[12];
     Sprite numbers_textures[8];
     Board game_board;
     RenderWindow window;
-    //FloatRect debugBounds;
     FloatRect faceBounds;
     void LoadNumbers() {
         for (int i = 0; i < 8; i++) {
@@ -398,33 +422,36 @@ private:
         }
     }
     void LoadDigits() { // digitOptions idx 0-9 is 0-9, 10 is sign
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 12; i++) {
             digits[i].setTexture(TextureManager::GetTexture("digits"));
         }
         digits[11].setTexture(TextureManager::GetTexture("digits"));
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 12; i++) {
             digits[i].setTextureRect(IntRect((i - 1) * 21, 0, 21, 32));
         }
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 12; i++) {
             digitOptions.push_back(digits[i]);
         }
     }
+    //load all member sprites
     void LoadAll() {
         LoadDigits();
         LoadNumbers();
         face.setTexture(TextureManager::GetTexture("face_happy"));
-        //debug.setTexture(TextureManager::GetTexture("debug"));
         mine.setTexture(TextureManager::GetTexture("mine"));
         flag.setTexture(TextureManager::GetTexture("flag"));
     }
+    //draws menu
     void DrawMenu() {
         digits[0].setPosition(Vector2f(0, (game_board.Get_Rows() * 32)));
         face.setPosition(Vector2f(game_board.Get_Cols() * 32 / 2 - 16, (game_board.Get_Rows() * 32)));
-        //debug.setPosition(Vector2f(numCols * 32 - (32 * 8), (numRows * 32)));
     }
 };
 
+
+/*MAIN: create game and run the loop*/
 int main() {
     Minesweeper game;
     game.GameLoop();
+    return 0;
 }
